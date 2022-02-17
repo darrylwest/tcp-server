2532bbcd229aa8dfcc89325d0c830dc63e6c7b12
darryl west <dpw@raincitysoftware.com>
2022-02-17 01:57:06 +0000

    split files into headers

---
+++ server.hpp
@@ -0,0 +1,44 @@
+
+#pragma once
+#ifndef TCP_SERVER_HPP
+#define TCP_SERVER_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+#include "session.hpp"
+
+using boost::asio::ip::tcp;
+
+class server {
+public:
+  server(boost::asio::io_context& io_context, short port) : io_context_(io_context),
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+    start_accept();
+  }
+
+private:
+  void start_accept() {
+    session* new_session = new session(io_context_);
+    acceptor_.async_accept(new_session->socket(),
+        boost::bind(&server::handle_accept, this, new_session,
+        boost::asio::placeholders::error));
+  }
+
+  void handle_accept(session* new_session, const boost::system::error_code& error) {
+    if (!error) {
+      new_session->start();
+    } else {
+      delete new_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_context& io_context_;
+  tcp::acceptor acceptor_;
+};
+
+#endif
---
+++ session.hpp
@@ -0,0 +1,56 @@
+
+#pragma once
+#ifndef TCP_SESSION_HPP
+#define TCP_SESSION_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+using boost::asio::ip::tcp;
+
+class session {
+public:
+  session(boost::asio::io_context& io_context) : socket_(io_context) { }
+
+  tcp::socket& socket() {
+    return socket_;
+  }
+
+  void start() {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
+    if (!error) {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&session::handle_write, this,
+            boost::asio::placeholders::error));
+    } else {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error) {
+    if (!error) {
+      socket_.async_read_some(boost::asio::buffer(data_, max_length),
+          boost::bind(&session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    } else {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+#endif
