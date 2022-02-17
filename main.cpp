7787719b4a743e149570b87ed953237360799736
darryl west <dpw@raincitysoftware.com>
2022-02-17 01:32:23 +0000

    added default port

--- main.cpp
+++ main.cpp
@@ -15,21 +15,15 @@
 
 using boost::asio::ip::tcp;
 
-class session
-{
+class session {
 public:
-  session(boost::asio::io_context& io_context)
-    : socket_(io_context)
-  {
-  }
+  session(boost::asio::io_context& io_context) : socket_(io_context) { }
 
-  tcp::socket& socket()
-  {
+  tcp::socket& socket() {
     return socket_;
   }
 
-  void start()
-  {
+  void start() {
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
         boost::bind(&session::handle_read, this,
           boost::asio::placeholders::error,
@@ -37,33 +31,24 @@ public:
   }
 
 private:
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred)
-  {
-    if (!error)
-    {
+  void handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
+    if (!error) {
       boost::asio::async_write(socket_,
           boost::asio::buffer(data_, bytes_transferred),
           boost::bind(&session::handle_write, this,
             boost::asio::placeholders::error));
-    }
-    else
-    {
+    } else {
       delete this;
     }
   }
 
-  void handle_write(const boost::system::error_code& error)
-  {
-    if (!error)
-    {
+  void handle_write(const boost::system::error_code& error) {
+    if (!error) {
       socket_.async_read_some(boost::asio::buffer(data_, max_length),
           boost::bind(&session::handle_read, this,
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
-    }
-    else
-    {
+    } else {
       delete this;
     }
   }
@@ -73,34 +58,26 @@ private:
   char data_[max_length];
 };
 
-class server
-{
+class server {
 public:
   server(boost::asio::io_context& io_context, short port)
     : io_context_(io_context),
-      acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-  {
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
     start_accept();
   }
 
 private:
-  void start_accept()
-  {
+  void start_accept() {
     session* new_session = new session(io_context_);
     acceptor_.async_accept(new_session->socket(),
         boost::bind(&server::handle_accept, this, new_session,
           boost::asio::placeholders::error));
   }
 
-  void handle_accept(session* new_session,
-      const boost::system::error_code& error)
-  {
-    if (!error)
-    {
+  void handle_accept(session* new_session, const boost::system::error_code& error) {
+    if (!error) {
       new_session->start();
-    }
-    else
-    {
+    } else {
       delete new_session;
     }
 
@@ -111,25 +88,19 @@ private:
   tcp::acceptor acceptor_;
 };
 
-int main(int argc, char* argv[])
-{
-  try
-  {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
-      return 1;
-    }
+int main(int argc, char* argv[]) {
+  try {
+    // parse command args for Config
+    int port = 2000;
 
     boost::asio::io_context io_context;
 
     using namespace std; // For atoi.
-    server s(io_context, atoi(argv[1]));
+    server s(io_context, port);
 
+    std::cout << "Start service at port: " << port << std::endl;
     io_context.run();
-  }
-  catch (std::exception& e)
-  {
+  } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << "\n";
   }
 
